cd69c489998536a58c7c79fa10e4415ce71dc40c
Himanshu Kumar Singh <97310760+to4to@users.noreply.github.com>
2023-12-02 22:35:42 +0530

    used log

--- test/FundMe.t.sol
+++ test/FundMe.t.sol
@@ -1,15 +1,18 @@
 //SPDX-Licence-Identifier:MIT
-pragma solidity 0.8.22;
+pragma solidity 0.8.19;
 
-import {Test} from "forge-std/Test.sol";
+import {Test,console} from "forge-std/Test.sol";
 
-contract FundMeTest is Test{
+contract FundMeTest is Test {
 
+    uint256 number=1;
+    function setUp() external {
+        number =2;
+    }
 
-
-
-
-
-
-
+    function testDemo() public {
+        //console.log(number);
+        //console.log("Hello!");  //if youwant to see console.log o/p run : forge test -vv
+        assertEq(number,2);
+    }
 }
