ac7cb853ae3621cbc783e5970e26548b1314990f
Himanshu Kumar Singh <97310760+to4to@users.noreply.github.com>
2023-12-04 02:23:12 +0530

    Starting Deploy Script

--- test/FundMe.t.sol
+++ test/FundMe.t.sol
@@ -1,18 +1,35 @@
 //SPDX-Licence-Identifier:MIT
 pragma solidity 0.8.19;
 
-import {Test,console} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
 
 contract FundMeTest is Test {
 
-    uint256 number=1;
+    FundMe fundMe;
+    // uint256 number=1;
     function setUp() external {
-        number =2;
+        // number =2;
+
+     fundMe =new FundMe();
     }
 
     function testDemo() public {
         //console.log(number);
         //console.log("Hello!");  //if youwant to see console.log o/p run : forge test -vv
-        assertEq(number,2);
+        // assertEq(number,2);
+    }
+
+
+    function testMinimumDollarIsFive() public{
+   assertEq(fundMe.MINIMUM_USD(),5e18);
+    }
+
+
+
+    function testOwnerIsMsgSender() public{
+            
+             assertEq(fundMe.i_owner(),address(this));
+
     }
 }
