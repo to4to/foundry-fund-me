fd23c0531630656e3c7b05f3facf0953b27d2b7c
Himanshu Kumar Singh <97310760+to4to@users.noreply.github.com>
2023-12-17 14:56:00 +0530

    Update DeployFundMe contract to inherit from Script and add run function

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -1,9 +1,13 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity 0.8.18;
+pragma solidity ^0.8.18;
 
 
 import{ Script} from "forge-std/Script.sol";
 import {FundMe} from "../src/FundMe.sol";
-contract DeployFundMe {
-    
+contract DeployFundMe is Script {
+    function run() external{
+        vm.startBroadcast();
+        new FundMe(msg.sender);
+        vm.stopBroadcast();
+    }
 }
